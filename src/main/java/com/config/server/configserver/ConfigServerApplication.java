97c76f4e00a968643434ad862e9c4c66e59b9e99
flymanflyman <406544970@qq.com>
2019-10-08 21:12:33 +0800

    2019.10.08.1

--- src/main/java/com/config/server/configserver/ConfigServerApplication.java
+++ src/main/java/com/config/server/configserver/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.config.server.configserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,6 +13,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+//		NONE - 应用程序不应作为Web应用程序运行，也不应启动嵌入式Web服务器。
+//		REACTIVE - 应用程序应作为响应式Web应用程序运行，并应启动嵌入式响应式Web服务器。
+//		SERVLET - 应用程序应作为基于servlet的Web应用程序运行，并应启动嵌入式servlet Web服务器。
+		new SpringApplicationBuilder(ConfigServerApplication.class).web(WebApplicationType.SERVLET).run(args);
+//		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
